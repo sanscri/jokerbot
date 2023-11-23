5c9f884c5ccab3260f475fd7bef187a4c5ea6a5c
Yana <y.kozhev@yandex.ru>
2023-11-24 02:18:39 +0700

    add buttons

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require('dotenv').config()
-const { Telegraf } = require("telegraf");
+const { Telegraf, Markup } = require("telegraf");
 const { message } = require('telegraf/filters')
 const Jimp = require("jimp");
 const axios = require('axios');
+const { createGzip } = require('zlib');
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('вы ввели не число, введите число.'))
 
 const isNumber = (userInput) => {
@@ -22,30 +21,58 @@ const addTextToImage = async (text, username, ctx) => {
 
     let loadedImage = await Jimp.read(process.env.PHOTO_URL);
     const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
-    loadedImage = loadedImage.rotate(-90).print(font, 80, 1100, text)
-    .print(font, 80, 1000, username).rotate(90);
+    loadedImage = loadedImage.rotate(-90).print(font, 80, 1100, text);
+    const font1 = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+    loadedImage = loadedImage.print(font1, 120, 1000, username).rotate(90);
     const buffer = await loadedImage.getBufferAsync("image/png");
     await ctx.replyWithPhoto({ source: buffer});
 }
+
 bot.on(message('text'), async (ctx) => {
-    // Using context shortcut
-    const message = ctx.message.text;
-
-    if(!isNumber(message)) {
-        return await ctx.reply("вы ввели не число, введите число.");
-    } 
-
-    if(checkCorrectNumber(Number(message))) {
-        const text = ctx.message.text;
-        const username = ctx.message.from.username;
-        const fileName = `./img/${username}.jpg`;
-    
-        await addTextToImage(text, '@' + username, ctx);
-    } else {
-        await ctx.reply("введите шестизначное число.");
+    if(ctx.message.text == "/start") {
+        return await ctx.reply("Приветствуем в нашем боте!");
     }
-  })
+    if(ctx.message.text === "получить билет" ) {
+        return await ctx.reply("введите в одном сообщении ник и 6 цифр через пробел.");
+    }
+    if(ctx.message.text != "получить билет" && ctx.message.text != "/start") {
+        const message = ctx.message.text.split(' ');
+        if(message.length != 2){
+            return await ctx.reply("вы ввели неккоректное количество параметров");
+        }
+        if(!isNumber(message[1])) {
+            return await ctx.reply("вторым параметром вы ввели не число, введите число.");
+        } 
+    
+        if(checkCorrectNumber(Number(message[1]))) {
+            const text = message[1];
+            const username = message[0];
+            await ctx.reply("Ваш билет генерируется, подождите какое-то время");
+            await addTextToImage(text, username, ctx);
+        } else {
+            await ctx.reply("введите шестизначное число.");
+        }
+    }
 
+})
+
+
+bot.command('start', async (ctx) => {
+    await ctx.reply("Приветсвтуем в нашем боте!");
+    return await ctx.reply('Привиет', Markup
+      .keyboard([
+        ['получить билет'],
+      ])
+      .oneTime()
+      .resize()
+    )
+  })
+/**
+ *  const handleMyCommand = (ctx) => {...}
+
+  bot.command('my-command', handleMyCommand)
+ */
+//bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.launch()
 
 // Enable graceful stop
