b6ed8777ae3a4f292521c11e2126acba18b17ab0
Yana <y.kozhev@yandex.ru>
2023-11-25 01:21:13 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 const { Telegraf, Markup } = require("telegraf");
 const { message } = require('telegraf/filters')
 const Jimp = require("jimp");
+const axios = require('axios');
 const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
- 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN)
 
 
@@ -33,38 +33,19 @@ const addTextToImage = async (text, username, ctx) => {
     let loadedImage = await Jimp.read(process.env.PHOTO_URL);
     //const font = await Jimp.loadFont(Jimp.FONT_SANS_64_BLACK);
     const font1 = await Jimp.loadFont('fonts/leaguespartan-bold/UmUBC09APAHdmWww4TzQtilV.ttf.fnt');
-    //const font1 = await Jimp.loadFont(process.env.FONT1);
+   // const font1 = await Jimp.loadFont(process.env.FONT1);
     loadedImage = loadedImage.rotate(-90).print(font1, 80, 1100, text);
     //const font1 = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
     const font2 = await Jimp.loadFont('fonts/CarroisGothicSC-Regular/WKPrnvBd9RtsiEmCBtSoKDiE.ttf.fnt');
-    //const font2 = await Jimp.loadFont(process.env.FONT2);
+   // const font2 = await Jimp.loadFont(process.env.FONT2);
     loadedImage = loadedImage.print(font2, 50, 1000, username).rotate(90);
     const buffer = await loadedImage.getBufferAsync("image/png");
     await ctx.replyWithPhoto({ source: buffer});
 }
 
 const addTextToImage1 = async (text, username, ctx) => {
-  const sX = 80, sY = 80;     // text position
-   
-  const img = await loadImage(process.env.PHOTO_URL);
-  const canvas = createCanvas(img.width, img.height);
-  const context = canvas.getContext("2d");
-  context.drawImage(img, 0, 0);
-  context.save();
-  context.translate( canvas.width - 1, 0 );
-  context.rotate(-Math.PI/2);
-  context.font = "64px serif bold";
-  context.fillStyle = "#5b5c3c";
-  context.textAlign = "center";
-  context.fillText(text, -220, -100 );
-  context.font = "32px serif";
-  context.textAlign = "center";
-  context.fillText(username, -220, -200);
-  context.restore();
-  const stream = canvas.createPNGStream();
-  await ctx.replyWithPhoto({ source: stream});
+   return ctx.reply(text);
 }
-
 bot.on(message('text'), async (ctx) => {
     if(ctx.message.text === "получить билет" ) {
         return await ctx.reply("введите в одном сообщении ник и 6 цифр через пробел.");
@@ -78,11 +59,13 @@ bot.on(message('text'), async (ctx) => {
             return await ctx.reply("вторым параметром вы ввели не число, введите число.");
         } 
     
+        console.log(message);
         if(checkCorrectNumber(Number(message[1]))) {
             const text = message[1];
             const username = message[0];
             await ctx.reply("Ваш билет генерируется, подождите какое-то время");
-            addTextToImage1(text, username, ctx);
+           // return await addTextToImage1(text, username, ctx);
+           await ctx.reply(text);
         } else {
             await ctx.reply("введите шестизначное число.");
         }
@@ -90,13 +73,10 @@ bot.on(message('text'), async (ctx) => {
 
 })
 
-/**
- * bot.launch()
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-*/
 
- exports.handler = async event => {
+/**
+ * 
+ * exports.handler = async event => {
   try {
     await bot.handleUpdate(JSON.parse(event.body));
     return { statusCode: 200, body: '' };
@@ -105,3 +85,10 @@ process.once('SIGTERM', () => bot.stop('SIGTERM'))
     return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
   }
 }
+
+
+bot.launch()
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+ */
